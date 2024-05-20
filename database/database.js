be2d50fc8221045792f107e06b09de7b54816708
Jose Carlos Camacho Salazar <50775349+J-Carlos-CS@users.noreply.github.com>
2024-05-20 00:06:47 -0400

    add

--- database/database.js
+++ database/database.js
@@ -1,6 +1,8 @@
 import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const sequelize = new Sequelize("CICEIDEV", "root", "root", {
+export const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   host: "localhost",
   dialect: "mysql",
 });
